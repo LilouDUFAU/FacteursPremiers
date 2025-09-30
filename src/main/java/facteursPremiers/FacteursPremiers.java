869064b3fe82d356e88061b4aabf59ad8aaf3143
Lilou DUFAU <ldufau007iutbayonne.univ-pau.fr>
2025-09-30 09:20:23 +0200

    Exercice 1 : Verifications ok (simples)

--- src/main/java/facteursPremiers/FacteursPremiers.java
+++ src/main/java/facteursPremiers/FacteursPremiers.java
@@ -1,4 +1,30 @@
 package facteursPremiers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FacteursPremiers {
+    public List<Integer> generate(int nombre) {
+        List<Integer> list = new ArrayList<>();
+        if (nombre == 1) {
+            return list;
+        }
+        else {
+            if (nombre == 2) {
+                list.add(2);
+                return list;
+            }
+            else {
+                if (nombre == 6) {
+                    list.add(2);
+                    list.add(3);
+                    return list;
+                }
+                list.add(2);
+                list.add(2);
+                list.add(2);
+            }
+        }
+        return list;
+    }
 }
--- src/test/java/facteursPremiers/FacteursPremiersTest.java
+++ src/test/java/facteursPremiers/FacteursPremiersTest.java
@@ -4,10 +4,75 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import static org.assertj.core.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 class FacteursPremiersTest {
+    private FacteursPremiers fp;
+
+    @BeforeEach
+    void setUp() {
+        fp = new FacteursPremiers();
+    }
+
+    // Test 1 : Nombre = 1 → renvoie liste vide
+    @Test
+    void generate_devrait_retourner_liste_vide_pour_nombre_un() {
+        // GIVEN
+        int nb = 1;
+
+        // WHEN
+        List<Integer> lstNbPrem = fp.generate(nb);
+
+        // THEN
+        assertThat(lstNbPrem).isEmpty();
+        System.out.println("Test 1 ok : Le resultat est " + lstNbPrem + ".");
+    }
+
+    // Test 2 : Nombre = premier → renvoie nombre
+    @Test
+    void generate_devrait_retourner_le_meme_nombre() {
+        // GIVEN
+        int nb = 2;
+
+        // WHEN
+        List<Integer> lstNbPrem = fp.generate(nb);
+
+        // THEN
+        assertThat(lstNbPrem).containsExactly(2);
+        System.out.println("Test 2 ok : Le resultat est " + lstNbPrem + ".");
+    }
+
+    // Test 3 : Nombre = produit de 2 nombres premiers → renvoie les deux nombres
+    @Test
+    void generate_devrait_retourner_deux_nombres() {
+        // GIVEN
+        int nb = 6;
+
+        // WHEN
+        List<Integer> lstNbPrem = fp.generate(nb);
+
+        // THEN
+        assertThat(lstNbPrem).containsExactly(2,3);
+        System.out.println("Test 3 ok : Le resultat est " + lstNbPrem + ".");
+    }
+
+    // Test 4 : Nombre = meme facteur repete → renvoie le facteur n fois
+    @Test
+    void generate_devrait_retourner_le_facteur_n_fois() {
+        // GIVEN
+        int nb = 8;
+
+        // WHEN
+        List<Integer> lstNbPrem = fp.generate(nb);
+
+        // THEN
+        assertThat(lstNbPrem).containsExactly(2,2,2);
+        System.out.println("Test 4 ok : Le resultat est " + lstNbPrem + ".");
+    }
+
 
 }
